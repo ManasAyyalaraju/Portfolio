6a318ce2d996d9f489642747be86033f3e701ae1
ManasAyyalaraju <84863848+ManasAyyalaraju@users.noreply.github.com>
2025-05-23 17:50:20 -0500

    Analytics Check

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -22,6 +23,7 @@ export default function RootLayout({
       >
         <Navbar />
         <main className="max-w-5xl mx-auto px-6 md:px-10">{children}</main>
+        <Analytics />
       </body>
     </html>
   );
